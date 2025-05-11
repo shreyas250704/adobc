9ddb258061d532207151f454c9d61c1688786ae7
shreyas564 <151796562+shreyas564@users.noreply.github.com>
2025-05-11 21:49:11 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -489,7 +489,7 @@ def main():
     # Add handlers
     app.add_handler(CommandHandler('start', start))
     app.add_handler(MessageHandler(filters.Text(["Hi", "hi", "HI"]), handle_hi))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_item_selection))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_item_selection))
     app.add_handler(CallbackQueryHandler(button))
     app.add_error_handler(error_handler)
 
